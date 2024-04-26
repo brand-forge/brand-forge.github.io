1c72683799f9c850aec4c549a587c892d3240a4d
ItsSitanshu <sitanshu15shrestha@gmail.com>
2024-04-26 11:59:40 +0545

    push

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,4 @@
-window.addEventListener('scroll', function() {
+function checkScroll() {
     var navbar = document.querySelector('.navbar');
     var logoImg = document.querySelector('.navbar .logo img');
     var logoSpan = document.querySelector('.navbar .logo span');
@@ -8,23 +8,13 @@ window.addEventListener('scroll', function() {
 
     if (window.scrollY > 20) {
         navbar.style.backgroundColor = '#ffffff';
-        setTimeout(function() {
-            logoSpan.style.color = 'transparent';
-            logoSpan.style.backgroundImage = 'linear-gradient(90deg, var(--color-callact) 0%, var(--color-middle) 25%, var(--color-middle-h2) 50%, var(--color-middle-h3) 75%, var(--color-middle-h4) 100%)';
-            logoImg.src = 'static/images/logo.svg';
-            optionsImg.src = 'static/images/ex-options.svg';
-            favicon.href = 'static/images/logo.svg';
-            underlineCol.style.textDecorationColor = "var(--color-middle-h4)";
-        }, 520);
+        underlineCol.style.textDecorationColor = "var(--color-middle-h4)";
     } else {
         navbar.style.backgroundColor = 'transparent';
-        setTimeout(function() {
-            logoSpan.style.color = 'transparent';
-            logoSpan.style.backgroundImage = 'linear-gradient(90deg, rgb(109, 109, 109) 0%, var(--color-neutral-2) 100%)';
-            logoImg.src = 'static/images/ex-logo.svg';
-            optionsImg.src = 'static/images/options.svg';
-            favicon.href = 'static/images/ex-logo.svg';
-            underlineCol.style.textDecorationColor = "rgb(255, 255, 255)";
-        }, 520);
+        underlineCol.style.textDecorationColor = "rgb(255, 255, 255)";
     }
-});
+}
+
+checkScroll();
+
+setInterval(checkScroll, 1);
